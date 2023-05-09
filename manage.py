823bfe4c375dad07120156343f556f9cdc94407d
YurijBielousovDnepr <gakayur@gmail.com>
2023-05-09 17:03:42 +0300

    9-05 настройка конфигов

--- manage.py
+++ manage.py
@@ -4,9 +4,12 @@ import os
 import sys
 
 
+sys.setrecursionlimit(1500)
+
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helpySamui.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
